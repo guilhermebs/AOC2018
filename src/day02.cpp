4c0da3839fe05d3240d596eae06412192e0b8aca
Guilherme Saturnino <guilherme.bicalho2@gmail.com>
2024-06-16 08:24:42 +0200

    day02, part 2

--- src/day02.cpp
+++ src/day02.cpp
@@ -31,10 +31,49 @@ void solve_part1() {
     
 }
 
+int compare_ids(std::string &id1, std::string &id2) {
+    if (id1.length() != id2.length())
+        return -1;
+    int differences = -1;
+    for (u_long i = 0; i < id1.length(); i++) {
+        if (id1[i] != id2[i] && differences == -1) {
+            differences = i;
+        } else if (id1[i] != id2[i] && differences != -1) {
+            return -1;
+        }
+    }
+    return differences;
+}
+
+void solve_part2() {
+    std::ifstream file("inputs/day02");
+    std::vector<std::string> box_ids;
+    std::string line;
+
+    while (std::getline(file, line)){
+        box_ids.push_back(line);
+    }
+
+    std::string solution = "";
+    for(auto id1: box_ids) {
+        for(auto id2: box_ids) {
+            int compare = compare_ids(id1, id2);
+            if (compare != -1) {
+                id1.erase(compare, 1);
+                solution = id1;
+                break;
+            }
+        }
+    }
+
+    std::cout << "Part 2 solution: " << solution << std::endl;
+    
+}
+
 int main() {
     auto started = std::chrono::high_resolution_clock::now();
     solve_part1();
-    //solve_part2();
+    solve_part2();
     auto done = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(done-started).count() << "ms\n";
 }
